79ca4375b2c1d86a4ec7434e7a710b4244e410d0
Peter Schulze <petschul@cisco.com>
2019-12-19 20:53:47 +0000

    initial commit

---
+++ index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const { WebexAdapter } = require('botbuilder-adapter-webex');
+const { Botkit } = require('botkit');
+
+dotenv.config();
+
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => res.send('Hello World!'));
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+const adapter = new WebexAdapter({
+  access_token: process.env.WEBEX_TEAMS_BOT_ACCESS_TOKEN,
+  enable_incomplete: true,
+  public_address: 'localhost:3000',
+});
+
+const controller = new Botkit({
+  adapter,
+  // ... other configuration options
+});
+
+async function myFunc() {
+  const bot = await controller.spawn();
+  await bot.startConversationInRoom(process.env.ROZBOT_ROOM_ID);
+  await bot.say("I'm watching you, Wazowski. Always watching. Always!");
+};
+
+myFunc();
