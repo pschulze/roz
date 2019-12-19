4aa2fab873657e43f557f705488048d631f254a5
Peter Schulze <petschul@cisco.com>
2019-12-19 23:42:38 +0000

    add command to display open prs for a repo

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import GitHub from 'github-api';
 
 const { WebexAdapter } = require('botbuilder-adapter-webex');
 const { Botkit } = require('botkit');
@@ -24,10 +25,38 @@ const controller = new Botkit({
   // ... other configuration options
 });
 
-async function myFunc() {
-  const bot = await controller.spawn();
-  await bot.startConversationInRoom(process.env.ROZBOT_ROOM_ID);
-  await bot.say("I'm watching you, Wazowski. Always watching. Always!");
-};
+controller.hears(new RegExp(/^github prs /), 'message', async(bot : any, message : any) => {
+  const gh = new GitHub(
+    { token: process.env.GITHUB_ACCESS_TOKEN },
+    process.env.GITHUB_API_BASE_URL
+  );
 
-myFunc();
+  const repoName = message.text.split(' ')[2];
+  const repo = gh.getRepo(process.env.GITHUB_ORG_NAME, repoName);
+
+  await repo.listPullRequests().then(({ data }) => {
+    let openPrs = data;
+    let reply : String;
+    if (openPrs.length === 0) {
+      reply = `There are no open PRs for ${repoName}.`
+    } else {
+      reply = `Here's all the open PRs for ${repoName}:\n`
+      openPrs.forEach(pr => {
+        reply += `- ${pr.title}\n`;
+        reply += `\t- ${pr.user.login}\n`;
+        reply += `\t- ${pr.html_url}\n`;
+      });
+    }
+    bot.say({ markdown: reply });
+  }).catch((error) => {
+    console.log(error.response.status);
+    if (error.response.status === 404) {
+      bot.say(`I can't find any repository named ${repoName}.`);
+    } else {
+      console.error(error);
+    }
+  });
+ });
+
+ controller.hears(new RegExp(/^tv /), 'message', async(bot : any, message : any) => {
+ });
